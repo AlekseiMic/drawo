95ccb982f7a15f9e4d6fcb0bb5d5758d5fc7846f
alesha <aleksey3ashitheg@gmail.com>
2023-03-03 23:35:41 +0700

    feat: setup cypress

--- plugins/drawer/tests/integration/init.cy.ts
+++ plugins/drawer/tests/integration/init.cy.ts
@@ -1,3 +1,21 @@
+import { Manager } from '../../../drawer';
+import { expect } from 'chai';
+
+declare global {
+  interface Window {
+    board: Manager;
+  }
+}
+
 describe('inits canvas', () => {
-  it('page has canvas element', () => {});
+  it('page has canvas element', () => {
+    cy.visit('http://localhost:4320');
+    cy.get('#app')
+      .trigger('mousedown', { x: 0, y: 0 })
+      .trigger('mousemove', { clientX: 1, clientY: 1 })
+      .trigger('mouseup', { clientX: 100, clientY: 600 });
+    cy.window().then((w) => {
+      expect(w.board.scratches.serialize().length).to.be.equal(1);
+    });
+  });
 });
