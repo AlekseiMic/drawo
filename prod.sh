fe0862f285985cd42e89e224858fca255427b8ec
alesha <aleksey3ashitheg@gmail.com>
2023-03-13 21:03:57 +0700

    feat: prod

---
+++ prod.sh
@@ -0,0 +1,39 @@
+CLIENT=/var/www/html/drawo.ru/client
+SERVER=/var/www/html/drawo.ru/server
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+
+echo "'${GREEN}'updating npm deps"
+if ! npm install > /dev/null 2>&1; then
+  echo "${RED}Could not update dependencies"
+  exit 1
+fi
+
+echo "'${GREEN}'building project"
+if ! npm run build > /dev/null 2>&1; then
+  echo "${RED}Build failed"
+  exit 1
+fi
+
+echo "${GREEN}********************"
+echo "${GREEN}* Build successful *"
+echo "${GREEN}********************"
+
+[ -d "${CLIENT}"] && rm -R $CLIENT
+[ -d "${SERVER}" ] && rm -R $SERVER
+
+mkdir $CLIENT
+mkdir $SERVER
+
+echo "'${GREEN}'copying client and server bundles"
+cp -R dist/client/* $CLIENT
+cp -R dist/server/* $SERVER
+
+rm /tmp/out
+echo "#!/usr/bin/env node"|cat - $SERVER/main.js > /tmp/out && mv /tmp/out $SERVER/main.js
+
+chmod +x $SERVER/main.js
+
+systemctl restart drawo
